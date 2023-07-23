867ee8a00baa4f7b4858ca877b276d5c7c163624
sulirb <121161440+sulirb@users.noreply.github.com>
2023-07-23 14:51:18 +0200

    responsive page accueil

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,31 +3,33 @@ import { Icon } from "@iconify/react";
 
 function Contact() {
   return (
-    <div className="cont">
-      <div className="cont__mail">
-        <h2>Contactez moi :</h2>
-        <h1>s.irbah59@gmail.com</h1>
-        <a href="mailto:s.irbah59@gmail.com">
-          <button className="cont__button">Envoyer un e-mail</button>
-        </a>
-      </div>
-      <div className="cont__rs">
-        <a
-          href="https://linkedin.com"
-          target="_blank"
-          rel="noreferrer"
-          className="cont__icon"
-        >
-          <Icon icon="mdi:linkedin" />
-        </a>
-        <a
-          href="https://github.com/sulirb"
-          target="_blank"
-          rel="noreferrer"
-          className="cont__icon"
-        >
-          <Icon icon="mdi:github" />
-        </a>
+    <div id="contact">
+      <div className="cont">
+        <div className="cont__mail">
+          <h2>Contactez moi :</h2>
+          <h1>s.irbah59@gmail.com</h1>
+          <a href="mailto:s.irbah59@gmail.com">
+            <button className="cont__button">Envoyer un e-mail</button>
+          </a>
+        </div>
+        <div className="cont__rs">
+          <a
+            href="https://www.linkedin.com/in/sullivan-irbah-270892284/"
+            target="_blank"
+            rel="noreferrer"
+            className="cont__icon"
+          >
+            <Icon icon="mdi:linkedin" />
+          </a>
+          <a
+            href="https://github.com/sulirb"
+            target="_blank"
+            rel="noreferrer"
+            className="cont__icon"
+          >
+            <Icon icon="mdi:github" />
+          </a>
+        </div>
       </div>
     </div>
   );
